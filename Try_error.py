f5517b318a1878de039f4986ce5e2e98b444c680
Ayush Kumar Vishwakarma <ayushkv0403@gmail.com>
2026-07-25 12:48:59 +0530

    Try_Except

--- Try_error.py
+++ Try_error.py
@@ -5,3 +5,15 @@ try:
     print("Hey the value of x is avilable in the database!")
 except:
     print("Hey what are you doing x is not defined!!!")
+
+
+try:
+    f = open("trial.txt")
+    try:
+        f.write("Hello Everyone")
+    except:
+        print("Something went wrong while writing in file")
+    finally:
+        f.close()
+except:
+    print("Something went wrong while opening the trail.txt file")
