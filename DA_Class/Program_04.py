3c3e729969acf0f9c82fd23909953ee96f600916
Ayush Kumar Vishwakarma <ayushkv0403@gmail.com>
2026-07-31 11:54:16 +0530

    Create Program_04.py

---
+++ DA_Class/Program_04.py
@@ -0,0 +1,30 @@
+string1 = "ayush is the number one coder in the world, and also one more thing is that he can solve almost every problem in the world"
+string2 = string1.replace("Ayush", "Rahul")
+print(string2)
+
+string3 = "Ayush"
+for i in string3:
+    print(i)
+
+string4 = string3.center(34)
+print(string4)
+
+string5 = string1.upper()
+print(string5)
+
+string6 = string1.capitalize()
+print(string6)
+
+string7 = string1.count("i")
+print(string7)
+
+string8 = string1.encode()
+print(string8)
+
+word = "Straße"
+
+print(word.lower())
+print(word.casefold())
+
+string9 = string1.endswith("d")
+print(string9)
