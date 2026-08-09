3d7b18349da0e0133da8d98377a6be3b257e7258
Ayush Kumar Vishwakarma <ayushkv0403@gmail.com>
2026-08-09 12:52:27 +0530

    Update User_Input.py

--- User_Input.py
+++ User_Input.py
@@ -1,14 +1,14 @@
-# print("Enter your name: ")
-# name = input()
-# print(f"Hello {name}, how are you?")
+print("Enter your name: ")
+name = input()
+print(f"Hello {name}, how are you?")
 
-# name = input("Enter you name: ")
-# print(f"Hello {name}")
+name = input("Enter you name: ")
+print(f"Hello {name}")
 
-# fav1 = input("What is your favorite animal: ")
-# fav2 = input("What is your favorite color: ")
-# fav3 = input("What is your favorite number: ")
-# print(f"Do you want a {fav2} {fav1} with {fav3} legs? ")
+fav1 = input("What is your favorite animal: ")
+fav2 = input("What is your favorite color: ")
+fav3 = input("What is your favorite number: ")
+print(f"Do you want a {fav2} {fav1} with {fav3} legs? ")
 
 
 import math 
