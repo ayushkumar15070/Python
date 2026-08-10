c7d297a9baf9c0b9d8b5845281db50ace3fc92f4
Ayush Kumar Vishwakarma <ayushkv0403@gmail.com>
2026-08-10 09:58:34 +0530

    Python_OOPs

---
+++ Python_Classes/Python_OOP.py
@@ -0,0 +1,27 @@
+# Python is an object-oriented programming language, allowing you to structure your code using classes and objects for better organization and reusability. 
+
+# Advantage of OOPs
+# 1. Provides a clear structure to programs
+# 2. Makes code easier to maintain, reuse, and debug
+# 3. Helps keep your code DRY( Don't Repeat Yourself)
+# 4. Allows you to build reusable applications with less code
+
+
+# example of OOPs in Python
+
+class Myclass:
+    x = 5
+    y = 10
+    z = 15
+
+
+
+p1 = Myclass()
+print(p1.x)
+
+p2 = Myclass()
+print(p2.x)
+print(p2.y)
+print(p2.z)
+
+print(p2.x * p2.y * p2.z)
