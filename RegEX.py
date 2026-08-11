cf4e33d1882035a0d2faad64422752c0894a4ecf
Ayush Kumar Vishwakarma <ayushkv0403@gmail.com>
2026-08-11 16:46:23 +0530

    Update RegEX.py

--- RegEX.py
+++ RegEX.py
@@ -16,3 +16,20 @@ print(x)
 
 z = re.split("\s", txt, 1)
 print(z)
+
+
+email = input("Enter your email: ")
+pattern = r"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$"
+
+if re.fullmatch(pattern, email):
+    print("Valid Email")
+else:
+    print("Invalid Email")
+
+number = input("Enter your number: ")
+numberpattern = r"[0-9]+"
+
+if re.fullmatch(numberpattern, number):
+    print("Valid Number")
+else:
+    print("Invalid Number")
