e53a899ab4e9cdc671da76fc33d24a14bc3f6607
Ayush Kumar Vishwakarma <ayushkv0403@gmail.com>
2026-08-17 10:34:59 +0530

    Update Python_Class_Methods.py

--- Python_Classes/Python_Class_Methods.py
+++ Python_Classes/Python_Class_Methods.py
@@ -24,3 +24,17 @@ c1 = Calculator()
 
 print(c1.addition(23, 44))
 print(c1.subtraction(5, 23))
+
+
+
+class person:
+    def __init__(self, name, age):
+        self.name = name 
+        self.age = age
+
+    def __str__(self):
+        return f"{self.name} {self.age}"
+
+
+p1 = person("Ayush", 20)
+print(p1)
