ad7b638745b120c09017d0c22dd0ae3c65ed01df
Ayush Kumar Vishwakarma <ayushkv0403@gmail.com>
2026-09-25 12:41:44 +0530

    Update Matplotlib_plotting.py

--- Python_Matplotlib/Matplotlib_plotting.py
+++ Python_Matplotlib/Matplotlib_plotting.py
@@ -1,24 +1,24 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-# x = np.array([1, 8])
-# y = np.array([3, 10])
+x = np.array([1, 8])
+y = np.array([3, 10])
 
-# plt.plot(x, y)
-# plt.show()
+plt.plot(x, y)
+plt.show()
 
 
 # we can plot as many points we want to
 
-# a = np.array([1, 2, 3, 4, 45, 6])
-# b = np.array([3, 4, 5, 6, 6, 73])
+a = np.array([1, 2, 3, 4, 45, 6])
+b = np.array([3, 4, 5, 6, 6, 73])
 
-# plt.plot(a, b)
-# plt.show()
+plt.plot(a, b)
+plt.show()
 
 
-# arr = [2, 3, 4, 5, 6,7]
-# arr2 = [3, 4, 5, 6, 7, 7]
+arr = [2, 3, 4, 5, 6,7]
+arr2 = [3, 4, 5, 6, 7, 7]
 
-# plt.plot(arr, arr2)
-# plt.show()
+plt.plot(arr, arr2)
+plt.show()
